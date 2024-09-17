708929a25327bc036ede2f850e47c1fdbaed8b62
GRACENOBLE <gracenoble72@gmail.com>
2024-09-17 14:09:45 +0300

    refactor(layout): changed metadata to reflect the apps true purpose

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "ToDo",
+  description: "The most convenient way to keep track of your tasks.",
 };
 
 export default function RootLayout({
